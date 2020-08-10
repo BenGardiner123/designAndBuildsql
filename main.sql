c16dd8b0c94384edf111746a135fa1e4046bc56b
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-08-10 10:19:15 +1000

    task 3 complete

--- main.sql
+++ main.sql
@@ -19,9 +19,18 @@ TOUR(TourName, Descriotion)
 PK: TourName */
 
 -- TASK 2 ----------------------------------------------------------
+/* DROP TABLE IF EXISTS BOOKING;
+DROP TABLE IF EXISTS EVENT;
+DROP TABLE IF EXISTS CLIENT;
+DROP TABLE IF EXISTS Tour;
+SELECT * 
+FROM sys.tables; 
+
+go   */
+ 
 
 
-/* CREATE TABLE TOUR (
+CREATE TABLE TOUR (
 TourName NVARCHAR(100), 
 Description NVARCHAR(500),
 PRIMARY KEY (TourName)
@@ -31,7 +40,7 @@ CREATE TABLE CLIENT (
 ClientID INT, 
 Surname NVARCHAR(100) not NULL, 
 GivenName NVARCHAR(100) not NULL, 
-Gender NVARCHAR(1) CHECK (Gender = 'M''F''I') NULL,
+Gender NVARCHAR(1) CHECK (Gender in ( 'M', 'F', 'I') )NULL,
 PRIMARY KEY (ClientID)
 );
 
@@ -61,11 +70,41 @@ FOREIGN KEY (ClientID) REFERENCES CLIENT,
 FOREIGN KEY (TourName, EventYear, EventMonth, EventDay) REFERENCES EVENT
 );
 
+go
 
 
 
+INSERT INTO TOUR(TourName, Description) VALUES  
+('North', 'Tour of wineries and outlets of the Bedigo and Castlemaine region'),
+('South', 'Tour of wineries and outlets of Mornington Penisula'),
+('West', 'Tour of wineries and outlets of the Geelong and Otways region');
 
-SELECT * 
-FROM sys.tables; */
+INSERT INTO CLIENT(ClientID, Surname, GivenName, Gender) VALUES
+('1','Price','Taylor','M'),
+('2','Gamble','Ellyse','F'),
+('3','Tan','Tilly','F'),
+('102151272', 'Gardiner', 'Ben', 'M');
 
--- ------------------------------------------------------------------------
+INSERT INTO EVENT(TourName, EventYear, EventMonth, EventDay,Fee) VALUES
+('North','2016', 'Jan','9','200'),
+('North','2016','Feb','13','225'),
+('South','2016','Jan','9','200'),
+('South','2016','Jan','16','200'),
+('West','2016','Jan','29','225');
+
+INSERT INTO BOOKING(ClientID, TourName, EventYear, EventMonth, EventDay, DateBooked, Payment) VALUES
+('1','North','2016','Jan',  '9', '2015-12-10',	'200'),
+('2','North','2016','Jan',	'9', '2015-12-16',	'200'),
+('1','North','2016','Feb',	'13', '2016-01-08',	'225'),
+('2','North','2016','Feb',	'13', '2016-01-14',	'125'),
+('3','North','2016','Feb',	'13', '2016-02-03',	'225'),
+('1','South','2016','Jan',	'9', '2015-12-10',	'200'),
+('2','South','2016','Jan',	'16', '2015-12-18',	'200'),
+('3','South','2016','Jan',	'16',  '2016-01-09','200'),
+('2','West','2016',	'Jan',	'29', '2015-12-17',	'225'),
+('3','West','2016',	'Jan',	'29', '2015-12-18',	'200');  
+
+GO
+ 
+SELECT *
+FROM client;  
